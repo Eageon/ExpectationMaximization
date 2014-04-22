c083a8fe7430638825363e5a03dddd853eca67eb
Eageon <yujun.eageon@gmail.com>
2014-04-22 15:09:43 -0500

    completed for EM

--- src/ComputeESS.java
+++ src/ComputeESS.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 
 public class ComputeESS {
 
-	ArrayList<ArrayList<ArrayList<Double>>> M;
+	ArrayList<ArrayList<Double>> M;
 	ArrayList<Variable> variables;
 	ArrayList<Factor> factors;
 	LinkedList<Evidence> evidenceSet;
@@ -21,21 +21,14 @@ public class ComputeESS {
 		for (int i = 0; i < model.factors.size(); i++) {
 			Factor factor = model.factors.get(i);
 			Variable var = factor.getVariable(factor.numScopes() - 1);
+
+			ArrayList<Double> MrepectVariableFactorStyle = new ArrayList<>(factor.table.size());
 			
-			int domainSize = var.domainSize();
-			ArrayList<ArrayList<Double>> MrepectVariable = new ArrayList<>(domainSize);
-			
-			for (int j = 0; j < domainSize; j++) {
-				ArrayList<Double> MvariableContent = new ArrayList<Double>(factor.table.size());
-				
-				for (int k = 0; k < factor.table.size(); k++) {
-					MvariableContent.add(0.0);
-				}
-				
-				MrepectVariable.add(MvariableContent);
+			for (int j = 0; j < factor.table.size(); j++) {			
+				MrepectVariableFactorStyle.add(0.0);
 			}
 			
-			M.add(MrepectVariable);
+			M.add(MrepectVariableFactorStyle);
 		}
 	}
 	
@@ -52,18 +45,18 @@ public class ComputeESS {
 			// for each variable
 			// for each factor is equivalent to for each variable
 			for (int i = 0; i < factors.size(); i++) {
-				ArrayList<ArrayList<Double>> MrepectVariable = M.get(i);
+				ArrayList<Double> MrepectVariableFactorStyle = M.get(i);
 				
 				Factor factor = factors.get(i);
 				Variable var = factor.getNodeVariable();
 				// for each pair of instantiation of xi and ui
 				for (int j = 0; j < factor.table.size(); j++) {
 					// TODO waiting for optimization
-					int[] indices = factor.tableIndexToVaraibleValue(j);
+					int[] values = factor.tableIndexToVaraibleValue(j);
 					
 					// if the x and u collide with o evidence, 
 					// I must not setSoftEvidence and return 0 immediately
-					if(om.isCollision(factor.variables, indices)) {
+					if(om.isCollision(factor.variables, values)) {
 						// make the function run 2^m completion, where m is the number of missing varaibles
 						continue;
 					}
@@ -71,18 +64,18 @@ public class ComputeESS {
 					// soft order and cluster should be computed before
 					// the evidence already set
 					// if not collide, the running 
-					model.setSoftEvidence(factor.variables, indices, false);
+					model.setSoftEvidence(factor.variables, values, false);
+					// must computer soft order before this execution
+					double result = model.runSoftProcess();
+					double prev = MrepectVariableFactorStyle.get(j);
 					
-					double result = model.softBucketElimination();
-					double prev = MrepectVariable.get(indices[indices.length - 1]).get(j / var.domainSize());
-					
-					MrepectVariable.get(indices[indices.length - 1]).set(j / var.domainSize(), prev + result);
+					MrepectVariableFactorStyle.set(j, prev + result);
 				}
 			}
 		}
 	}
 	
-	public ArrayList<ArrayList<ArrayList<Double>>> runComputeESS() {
+	public ArrayList<ArrayList<Double>> runComputeESS() {
 		if(null == model) {
 			return null;
 		}
--- src/ExpectationMaximization.java
+++ src/ExpectationMaximization.java
@@ -1,58 +1,53 @@
 import java.util.ArrayList;
 
-
 public class ExpectationMaximization {
-	
+
 	public int convergenceCondition = 20;
-	
+
 	public GraphicalModel model;
-	
+
 	// The table of Factor should be initialized before executing this function
 	public void initializeParameters(GraphicalModel model) {
 		this.model = model;
-		
+
 	}
-	
+
 	public GraphicalModel runExpectationMaximization() {
 		for (int i = 0; i < convergenceCondition; i++) {
 			ComputeESS computeESS = new ComputeESS();
 			computeESS.initializeDataStructure(model);
-			
-			ArrayList<ArrayList<ArrayList<Double>>> M = computeESS.runComputeESS();
-			
-			if(null == M) {
+
+			ArrayList<ArrayList<Double>> M = computeESS.runComputeESS();
+
+			if (null == M) {
 				System.out.println("Error: runComputeESS()");
 				System.exit(0);
 			}
-			
+
 			for (int j = 0; j < M.size(); j++) {
 				Factor factor = model.getFactor(j);
 				Variable var = factor.getNodeVariable();
-				
-				ArrayList<ArrayList<Double>> MrespectVariable = M.get(j);
-				
+
+				ArrayList<Double> MrespectVariableFactorStyle = M.get(j);
+
 				// calculate the M[ui], sum the M[xi, ui]
-				double[] M_ui = new double[MrespectVariable.get(0).size()];
-				for (int k = 0; k < MrespectVariable.size(); k++) {
-					ArrayList<Double> Mvar = MrespectVariable.get(k);
-					
-					for (int l = 0; l < Mvar.size(); l++) {
-						M_ui[k] += Mvar.get(l);
-					}
+				double[] M_ui = new double[MrespectVariableFactorStyle.size()
+						/ var.domainSize()];
+				for (int k = 0; k < MrespectVariableFactorStyle.size(); k++) {
+					double Mvar = MrespectVariableFactorStyle.get(k);
+					M_ui[k / var.domainSize()] += Mvar;
 				}
-				
+
 				// for each instantiation of xi
-				for (int k = 0; k < MrespectVariable.size(); k++) {
-					ArrayList<Double> Mvar = MrespectVariable.get(k);
-					
+				for (int k = 0; k < MrespectVariableFactorStyle.size(); k++) {
+					double Mvar = MrespectVariableFactorStyle.get(k);
+
 					// for each instantiation of ui
-					for (int l = 0; l < Mvar.size(); l++) {
-						
-					}
+					factor.setTableValue(k, Mvar / M_ui[k / var.domainSize()]);
 				}
 			}
 		}
-		
+
 		return model;
 	}
 
